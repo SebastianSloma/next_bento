f84b0f02de5756caff52001a191d8ffe81737c43
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-27 09:26:07 +0100

    z

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Footer from '@/components/Footer';
 import LeftPanel from '@/components/LeftPanel';
-// import RightPanel from "@/components/right-panel";
+import RightPanel from "@/components/RightPanel";
 
 export default function Home() {
 	return (
@@ -10,11 +10,11 @@ export default function Home() {
 			{/* Overlay */}
 			<div className='absolute inset-0 z-10 bg-gradient-to-b from-white via-white/90 to-white dark:from-neutral-950 dark:via-neutral-950/90 dark:to-neutral-950' />
 			<div className='container relative z-20 flex flex-col w-full h-full px-6 gap:6 xl:gap-10 xl:flex-row'>
-				{/* Left Panel */}
+	
 				<LeftPanel />
-				{/* Right Panel */}
-				{/* <RightPanel /> */}
-				{/* Footer for Mobile */}
+				
+				<RightPanel />
+				
 				<div className='flex pb-6 xl:hidden'>
 					<Footer />
 				</div>
